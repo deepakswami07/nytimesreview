489140f613522ae5113c9c89f8859adced84af5a
daniel <deepakswami07@gmail.com>
2016-05-20 17:10:18 -0400

    add call data

--- js/film.js
+++ js/film.js
@@ -27,6 +27,7 @@ var filmReview = function(){
                 console.log(num_results);
                 console.log(title);
                 console.log(summary);
+                console.log(data);
             }).fail(function(err) {
             throw err;
         });
@@ -35,32 +36,5 @@ var filmReview = function(){
 }
 
 filmReview();
-
-
-/* var url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
-url += '?' + $.param({
-  'api-key': "9cced63c6fde5aa51d72ec61a881f5e3:19:71855090",
-  'query': "Manhattan night",
-  'critics-pick': "N";
-
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(data) {
-    var status = data.status;
-    var copyright = data.copyright;
-    var num_results = data.num_results;
-    var title = data.results[0].display_title;
-    var summary = data.results[0].summary_short
-    console.log(status);
-    console.log(copyright);
-    console.log(num_results);
-    console.log(title);
-    console.log(summary);
-}).fail(function(err) {
-  throw err;
-});
-
-*/
 
 
