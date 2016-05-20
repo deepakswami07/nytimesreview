ef1b28373ee4918d109631edbca7fd95cfebe912
daniel <deepakswami07@gmail.com>
2016-05-20 00:41:21 -0400

    new files added to project

---
+++ js/getReviews.js
@@ -0,0 +1,7 @@
+/////////////////////////////////////////
+// begin navbar collapse on selection ///
+$('.navbar-collapse').click('li', function() {
+    $('.navbar-collapse').collapse('hide');
+});
+// end navbar collapse on selection ///
+///////////////////////////////////////
